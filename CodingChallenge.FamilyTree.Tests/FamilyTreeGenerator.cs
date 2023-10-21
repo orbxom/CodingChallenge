df83686dab618d1449831fd09723c74d605c1ed0
Zachery Knowles <Zknowles@outlook.com>
2023-10-21 11:14:35 +1100

    Updated the family tree generator to make sure that it always generates a tree large enough for the tests to work correctly.

--- CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
+++ CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
@@ -13,10 +13,11 @@ namespace CodingChallenge.FamilyTree.Tests
             var hierarchySpec = Builder<HierarchySpec<Person>>.CreateNew()
                 .With(h => h.AddMethod, (p1, p2) => p1.Descendants.Add(p2))
                 .With(h => h.Depth = 7)
-                .With(h => h.MaximumChildren = 3)
+                .With(h => h.MinimumChildren = 2)
+                .With(h => h.MaximumChildren = 4)
                 .With(h => h.NumberOfRoots = 1).Build();
 
-            var person = Builder<Person>.CreateListOfSize(1100).BuildHierarchy(hierarchySpec).First();
+            var person = Builder<Person>.CreateListOfSize(5461).BuildHierarchy(hierarchySpec).First();
 
             return person;
         }
